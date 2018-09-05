24150976d4866f15bbc09e1290634e0e92c1fcb0
Kenneth Chidozie <kenzdozz@gmail.com>
2018-09-05 21:05:27 +0100

    Setting up the frontend

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const apiRouter = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/../public')));
 
 app.use((req, res, next) => {
   res.locals.user = {};
