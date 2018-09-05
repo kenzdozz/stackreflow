c322ceafae8bc8e6c9ea03c165dffc40cf319ef1
Kenneth Chidozie <kenzdozz@gmail.com>
2018-09-05 22:46:08 +0100

    Setting up the frontend

--- src/Model/Question.js
+++ src/Model/Question.js
@@ -115,6 +115,7 @@ class Question {
     const getQuery = 'SELECT questions.*, users.name AS username FROM questions LEFT JOIN users ON users.id = questions.user_id ORDER BY questions.created_at DESC';
 
     pool.connect((error, client, done) => {
+      console.log(error);
       if (error) return callback({ status: false, message: error.stack });
       return client.query(getQuery, (err, res) => {
         console.log(err);
