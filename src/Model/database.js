bd35152219af8c8f7fd892a1b6e58b26457ef4f4
Kenneth Chidozie <kenzdozz@gmail.com>
2018-09-05 23:14:43 +0100

    Setting up the frontend

--- src/Model/database.js
+++ src/Model/database.js
@@ -1,5 +1,7 @@
 import { Pool } from 'pg';
 
-const pool = new Pool();
+const connectionString = `postgres://diabpyfbimjuya:c50f60c6ad2f56d26aa449bc5814a3323d4a6e32e544ce26619a4f9868618e18@ec2-54-227-244-12.compute-1.amazonaws.com:5432/d233ill936cr99`;
+
+const pool = new Pool(connectionString);
 
 export default pool;
