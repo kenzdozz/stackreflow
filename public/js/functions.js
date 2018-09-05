46cf0356404ff5046fd21553ed0229f8bab196c7
Kenneth Chidozie <kenzdozz@gmail.com>
2018-09-05 20:19:55 +0100

    Setting up the frontend

---
+++ public/js/functions.js
@@ -0,0 +1,50 @@
+
+function fetchCall(url, method, data, callback) {
+    let config = {
+        method,
+        mode: 'cors',
+        dataType: "json",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            'x-access-token': localStorage.getItem('userToken'),
+        },
+    };
+    if (data) config.body = JSON.stringify(data);
+    fetch(url, config).then((resData) => {
+        return resData.json();
+    }).then(response => {
+        callback(null, response);
+    }).catch(err => {
+        callback(err, null);
+    });
+}
+
+function handleBtn(authCheck) {
+    const loginBtn = document.querySelector('.nav .login');
+    const signupBtn = document.querySelector('.nav .signup');
+    if (authCheck) {
+        loginBtn.setAttribute('href', 'user.html');
+        loginBtn.innerHTML = 'My Account';
+        loginBtn.classList.remove('hide');
+        signupBtn.setAttribute('href', 'logout.html');
+        signupBtn.innerHTML = 'Logout';
+        signupBtn.classList.remove('hide');
+    } else {
+        loginBtn.classList.remove('hide');
+        signupBtn.classList.remove('hide');
+    }
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function hasClass(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
+}
